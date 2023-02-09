6d49bfb0c7dd7e5f54ad175353e04b61027775a5
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-02-09 17:46:09 -0300

    service methods written

--- finding_chart/src/interface_adapters/repos/object_repo.py
+++ finding_chart/src/interface_adapters/repos/object_repo.py
@@ -2,8 +2,8 @@ from use_cases.interfaces.object_interface import IObjectRepo
 
 
 class ObjectRepo(IObjectRepo):
-    def get_stats(self, api, oid, output_format):
-        stats = api.query_objects(oid=oid, format="pandas")
+    def get_stats(self, api, oid, format):
+        stats = api.query_objects(oid=oid, format=format)
         magstats = api.query_magstats(oid=oid)
         stats = stats.iloc[0]
         stats["mean_magpsf_r"] = magstats[1]["magmean"]
---
+++ finding_chart/src/use_cases/services/service.py
@@ -0,0 +1,67 @@
+import numpy as np
+import base64
+import matplotlib.pyplot as plt
+from io import BytesIO
+import PIL.ImageOps
+from entities.figure_model import FigureModel
+from astropy.coordinates import ICRS
+from astropy import units as u
+from astropy.time import Time
+
+
+def get_object_stats(object_repo, api, oid, format):
+    return object_repo.get_stats(api, oid, format)
+
+
+def get_gray_img(
+    image_repo, PANSTARR_FILE_PATH, PANSTARR_CUTOUT_PATH, ra, dec, size, output_size
+):
+    return image_repo.get_gray_img(
+        PANSTARR_FILE_PATH, PANSTARR_CUTOUT_PATH, ra, dec, size, output_size
+    )
+
+
+def img_to_np_array(img):
+    img = PIL.ImageOps.invert(img)
+    img = np.asarray(img)
+    return img
+
+
+def get_figure(img, stats, size):
+    model = FigureModel(img, stats, size)
+    model.create_figure()
+    fig = model.add_figure_text()
+    return fig
+
+
+def fig_img_to_string(figure):
+    buf = BytesIO()
+    figure.savefig(buf, format="jpg", bbox_inches="tight", transparent=True)
+    buf.seek(0)
+    im = buf.read()
+    img_str = base64.b64encode(im).decode("utf-8")
+
+    return img_str
+
+
+def get_ICRS_coords(stats):
+    coords = ICRS(stats.meanra * u.degree, stats.meandec * u.degree)
+    ra = coords.ra.to_string(u.hour)
+    dec = coords.dec.to_string()
+
+    return ra, dec
+
+
+def format_first_and_last_detection(stats):
+    stats["first_detection"] = Time(stats["firstmjd"], format="mjd")
+    stats["first_detection"].format = "datetime"
+    stats["last_detection"] = Time(stats["lastmjd"], format="mjd")
+    stats["last_detection"].format = "datetime"
+
+
+def get_chart_image(img, stats, size):
+    img_array = img_to_np_array(img)
+    fig = get_figure(img_array, stats, size)
+    img_str = fig_img_to_string(fig)
+
+    return img_str
