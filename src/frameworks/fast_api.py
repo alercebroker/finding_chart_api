8925a705928b03ab06c25b26d4ef86867d5b5792
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-02-13 10:33:14 -0300

    fastAPI view created

---
+++ src/frameworks/fast_api.py
@@ -0,0 +1,38 @@
+import os
+import uvicorn
+from fastapi import FastAPI, Query, Depends
+
+from src.interface_adapters.controllers.controller import (
+    controller_get_chart,
+)
+
+from pydantic.dataclasses import dataclass
+
+app = FastAPI(
+    description="Simple findingchart for ZTF objects.",
+    title="ALeRCE Finding Chart API",
+    root_path=os.getenv("ROOT_PATH", "/"),
+)
+
+
+STATIC_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
+CSS_PATH = [
+    os.path.join(STATIC_PATH, "css/template.css"),
+    os.path.join(STATIC_PATH, "css/bootstrap.min.css"),
+]
+
+logo_path = os.path.join(STATIC_PATH, "img/logo.png")
+
+
+@app.get("/")
+def index():
+    return "ALeRCE Finding Chart Generator"
+
+
+@app.get("/get_chart")
+def get_chart():
+    return controller_get_chart(params, logo_path)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5001)
