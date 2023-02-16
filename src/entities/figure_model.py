8f9c04c2fb95e5f97b4ed57afbdf2f41ed071e7e
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-02-16 10:52:14 -0300

    moved one domain method to the figure_model

--- src/entities/figure_model.py
+++ src/entities/figure_model.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from io import BytesIO
+import base64
 
 
 class FigureModel:
@@ -9,42 +11,48 @@ class FigureModel:
         self.size = size
 
     def create_figure(self):
-        self.fig, self.axes = plt.subplots(1, figsize=(9, 9))
-        self.axes.matshow(self.img, cmap="Greys_r", interpolation="nearest")
+        fig, axes = plt.subplots(1, figsize=(9, 9))
+        axes.matshow(self.img, cmap="Greys_r", interpolation="nearest")
         self.color = "blue"
         alpha = 0.3
         # Adding cross
         #              --------------------modelo---------------------
-        self.axes.plot([0, 300], [351, 351], c=self.color, alpha=alpha)
-        self.axes.plot([701, 401], [351, 351], c=self.color, alpha=alpha)
-        self.axes.plot([351, 351], [0, 300], c=self.color, alpha=alpha)
-        self.axes.plot([351, 351], [701, 401], c=self.color, alpha=alpha)
+        axes.plot([0, 300], [351, 351], c=self.color, alpha=alpha)
+        axes.plot([701, 401], [351, 351], c=self.color, alpha=alpha)
+        axes.plot([351, 351], [0, 300], c=self.color, alpha=alpha)
+        axes.plot([351, 351], [701, 401], c=self.color, alpha=alpha)
 
-        self.axes.arrow(
-            701 - 30, 701 - 10, 0, -50, shape="full", width=3, color=self.color
-        )
-        self.axes.arrow(
-            701 - 30, 701 - 10, -50, 0, shape="full", width=3, color=self.color
-        )
-        self.axes.text(701 - 110, 701 - 5, "E", color=self.color)
-        self.axes.text(701 - 34, 701 - 80, "N", color=self.color)
+        axes.arrow(701 - 30, 701 - 10, 0, -50, shape="full", width=3, color=self.color)
+        axes.arrow(701 - 30, 701 - 10, -50, 0, shape="full", width=3, color=self.color)
+        axes.text(701 - 110, 701 - 5, "E", color=self.color)
+        axes.text(701 - 34, 701 - 80, "N", color=self.color)
+        return fig, axes
 
-    def add_figure_text(self):
+    def add_figure_text(self, fig, axes):
         # Text
         string = "PanSTARRS DR1\nra: {} dec: {}\nscale: 0.25 arcsec/pix\nfield size: {} arcsec".format(
             np.round(self.stats.meanra, 5),
             np.round(self.stats.meandec, 5),
             self.size * 0.25,
         )
-        self.axes.text(
+        axes.text(
             0.02,
             0.98,
             string,
             color=self.color,
-            transform=self.axes.transAxes,
+            transform=axes.transAxes,
             fontsize=9,
             verticalalignment="top",
         )
-        self.axes.axis("off")
+        axes.axis("off")
 
-        return self.fig
+        return fig
+
+    def fig_img_to_string(self, figure):
+        buf = BytesIO()
+        figure.savefig(buf, format="jpg", bbox_inches="tight", transparent=True)
+        buf.seek(0)
+        im = buf.read()
+        img_str = base64.b64encode(im).decode("utf-8")
+
+        return img_str
